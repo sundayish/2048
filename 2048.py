c5dc5d54f6987c565e6d3b114ed151f8c5c3d032
Tom Sonntag <tm.snntag@icloud.com>
2020-10-15 10:42:12 +0200

    Add: Basic GUI, 4x4 grid and score

--- 2048.py
+++ 2048.py
@@ -1,6 +1,48 @@
 import tkinter as tk
+import colors as c
 
 class Game(tk.Frame):
-    tk.Frame.__init__(self)
-    self.grid()
-    self.master.title("2048")
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title("2048")
+
+        self.main_grid = tk.Frame(
+            self, bg=c.GRID_COLOR, bd=3, width=600, height=600
+        )
+
+        self.main_grid.grid(pady=(100, 0))
+        self.make_GUI()
+
+        self.mainloop()
+    
+    def make_GUI(self):
+        # Make 4x4 grid
+        self.cells = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg=c.EMPTY_CELL_COLOR,
+                    width=150,
+                    height=150
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number= tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+        
+        # Make score
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=45, anchor="center")
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=c.SCORE_LABEL_FONT
+        ).grid(row=0)
+        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
+        self.score_label.grid(row=1)
+
